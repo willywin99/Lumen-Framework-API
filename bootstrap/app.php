32bae0df155172818c0d686b54489620c1faa150
willywin99 <willywinsianturi@gmail.com>
2021-01-23 17:33:08 +0700

    Menggunakan Middleware pada Route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,9 +74,10 @@ $app->configure('app');
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+    'age' => App\Http\Middleware\Age::class
+]);
 
 /*
 |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -22,67 +22,75 @@ $router->get('/key', function() {
     return Str::random(32);
 });
 
-$router->get('/foo', function() {
-    return 'Hello, Get Method!';
-});
-
-$router->post('/bar', function() {
-    return 'Hello, POST Method!';
-});
-
-// The router allows you to register routes that respon to any HTTP verb:
-$router->get('/get', function() {
-    return 'GET';
-});
-
-$router->get('/post', function() {
-    return 'POST';
-});
-
-$router->get('/put', function() {
-    return 'PUT';
-});
-
-$router->get('/patch', function() {
-    return 'PATCH';
-});
-
-$router->get('/delete', function() {
-    return 'DELETE';
-});
-
-$router->get('/options', function() {
-    return 'OPTIONS';
-});
-
-// Basic Route Parameter
-$router->get('/user/{id}', function($id) {
-    return 'User id = ' . $id;
-});
-
-$router->get('/post/{postId}/comments/{commentId}', function ($postId, $commentId) {
-    return "Post ID =  {$postId} <br> Comment ID = {$commentId}";
-});
-
-// Optional Route Parameter
-$router->get('/optional[/{param}]', function ($param = null) {
-    return $param;
-});
-
-$router->get('profile/willywin', ['as' => 'route.profile', function () {
-    return 'Route willywin';
+$router->get('/admin/home', ['middleware' => 'age', function() {
+    return 'Old Enough';
 }]);
 
-$router->get('profile', function () {
-    return redirect()->route('route.profile');
+$router->get('/fail', function () {
+    return 'Not yet mature';
 });
 
-$router->group(['prefix' => 'admin'], function() use ($router) {
-    $router->get('home', function() {
-        return 'Home Admin';
-    });
+// $router->get('/foo', function() {
+//     return 'Hello, Get Method!';
+// });
 
-    $router->get('profile', function() {
-        return 'Profile Admin';
-    });
-});
+// $router->post('/bar', function() {
+//     return 'Hello, POST Method!';
+// });
+
+// // The router allows you to register routes that respon to any HTTP verb:
+// $router->get('/get', function() {
+//     return 'GET';
+// });
+
+// $router->get('/post', function() {
+//     return 'POST';
+// });
+
+// $router->get('/put', function() {
+//     return 'PUT';
+// });
+
+// $router->get('/patch', function() {
+//     return 'PATCH';
+// });
+
+// $router->get('/delete', function() {
+//     return 'DELETE';
+// });
+
+// $router->get('/options', function() {
+//     return 'OPTIONS';
+// });
+
+// // Basic Route Parameter
+// $router->get('/user/{id}', function($id) {
+//     return 'User id = ' . $id;
+// });
+
+// $router->get('/post/{postId}/comments/{commentId}', function ($postId, $commentId) {
+//     return "Post ID =  {$postId} <br> Comment ID = {$commentId}";
+// });
+
+// // Optional Route Parameter
+// $router->get('/optional[/{param}]', function ($param = null) {
+//     return $param;
+// });
+
+// $router->get('profile/willywin', ['as' => 'route.profile', function () {
+//     return 'Route willywin';
+// }]);
+
+// $router->get('profile', function () {
+//     return redirect()->route('route.profile');
+// });
+
+// $router->group(['prefix' => 'admin', 'middleware' => 'auth', 'namespace' => ''], function() use ($router) {
+//     $router->get('home', function() {
+//         return 'Home Admin';
+//     });
+
+//     $router->get('profile', function() {
+//         return 'Profile Admin';
+//     });
+// });
